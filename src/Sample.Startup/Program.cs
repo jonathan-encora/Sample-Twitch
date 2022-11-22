ba62d4673b6cd48176c9f134cbf020951fde1bbc
Jonathan Arce <Jonathan Arce@DESKTOP-IB84VEI>
2022-11-22 09:46:45 -0800

    No errors update

---
+++ src/Sample.Startup/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Sample.Startup
+{
+    using MassTransit;
+    using Microsoft.Extensions.DependencyInjection;
+    using Sample.Components.BatchConsumers;
+    using Sample.Components.Consumers;
+    using Sample.Components.CourierActivities;
+    using Sample.Components.StateMachines;
+    using Sample.Components.StateMachines.OrderStateMachineActivities;
+    using Warehouse.Contracts;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var services = new ServiceCollection();
+
+            services.AddScoped<AcceptOrderActivity>();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
+                x.AddActivitiesFromNamespaceContaining<AllocateInventoryActivity>();
+                x.AddConsumersFromNamespaceContaining<RoutingSlipBatchEventConsumer>();
+                x.AddSagaStateMachine<OrderStateMachine, OrderState>()
+                    .MongoDbRepository(r =>
+                    {
+                        r.Connection = "mongodb://mongo";
+                        r.DatabaseName = "allocations";
+                    });
+                x.AddRequestClient<AllocateInventory>();
+            });
+        }
+    }
+}
---
+++ src/Warehouse.Startup/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Warehouse.Startup
+{
+    using MassTransit;
+    using Microsoft.Extensions.DependencyInjection;
+    using Sample.Components.StateMachines;
+    using Warehouse.Components.Consumers;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var services = new ServiceCollection();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumersFromNamespaceContaining<AllocateInventoryConsumer>();
+                x.AddSagaStateMachine<OrderStateMachine, OrderState>()
+                    .MongoDbRepository(r =>
+                    {
+                        r.Connection = "mongodb://mongo";
+                        r.DatabaseName = "allocations";
+                    });
+            });
+        }
+    }
+}
